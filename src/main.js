3d4bb5dcc75adebb90733efee728f11c70f8bef3
Kelly Osena <52971852+osenak@users.noreply.github.com>
2025-03-10 00:46:11 -0600

    implemented router for future use

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount('#app')
+// Create the Vue app instance
+const app = createApp(App);
+
+// Use the router with the app
+app.use(router);
+
+// Mount the app to the DOM element with id="app" in index.html
+app.mount("#app");
